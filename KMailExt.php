741741dbe3ef771af908fb3d27794c28c1f90448
amnah <amnahx@gmail.com>
2013-02-22 20:22:08 -0500

    reorganize the main class file and add extended file example

---
+++ KMailExt.php
@@ -0,0 +1,32 @@
+<?php
+
+// require base class
+Yii::app()->import("application.extensions.kmail.KMail");
+
+class KMailExt extends Kmail {
+
+    /**
+     * Gets template of a swift mailer message with default settings
+     * @return Swift_Mime_Message
+     */
+    public function getMessageTemplate() {
+        $message = Swift_Message::newInstance();
+        $message->setFrom(array(Yii::app()->params["adminEmail"] => "admin"));
+        $message->setContentType("text/html");
+        $message->setCharset("utf-8");
+        return $message;
+    }
+
+    /**
+     * Sends activation email to user after registration
+     * @param $user
+     * @param $profile
+     */
+    public function sendEmailActivationKey($user, $profile) {
+        $message = $this->getMessageTemplate();
+        $message->setSubject('Registration Activation');
+        $message->setBody($this->renderView("activation", array("user"=>$user, "profile"=>$profile)));
+        $message->setTo($user->email);
+        $this->send($message);
+    }
+}
